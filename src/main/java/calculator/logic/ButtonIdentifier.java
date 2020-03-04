7e42a4949ee49a156a0baad058bdf29950d77f3e
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-03-04 13:41:41 +0100

    Add enum for button identification

---
+++ src/main/java/calculator/logic/ButtonIdentifier.java
@@ -0,0 +1,37 @@
+package calculator.logic;
+
+public enum ButtonIdentifier {
+
+    ZERO('0'),
+    ONE('1'),
+    TWO('2'),
+    THREE('3'),
+    FOUR('4'),
+    FIVE('5'),
+    SIX('6'),
+    SEVEN('7'),
+    EIGHT('8'),
+    NINE('9'),
+    COMMA(','),
+    ADD('+'),
+    SUBTRACT('-'),
+    MULTIPLY('*'),
+    DIVIDE('/'),
+    BACKSPACE('\b'),
+    CLEAR_ALL('c'),
+    CLEAR('c'),
+    NEGATE('\u00B1'),
+    EQUALS('=');
+
+
+    char character;
+
+    ButtonIdentifier(char character) {
+        this.character = character;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+}
