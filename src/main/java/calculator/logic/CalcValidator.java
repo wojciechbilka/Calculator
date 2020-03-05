6150727fd10aec0fc74476660cf4dbfd9665a83d
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-03-05 14:48:30 +0100

    Update Validator for new data type (Equation)

--- src/main/java/calculator/logic/CalcValidator.java
+++ src/main/java/calculator/logic/CalcValidator.java
@@ -1,74 +1,130 @@
 package calculator.logic;
 
+import java.util.ArrayList;
+
 public class CalcValidator {
 
     private StringBuilder result = new StringBuilder();
     private Character lastChar;
+    private Equation currentEquation;
+
 
     public CalcValidator() {
+        this.currentEquation = new Equation();
     }
 
-    public CalcValidator(StringBuilder result) {
+    public CalcValidator(StringBuilder result, Equation currentEquation) {
         this.result = result;
+        this.currentEquation = currentEquation;
     }
 
     public String getResult() {
-        return result.toString();
+        return currentEquation.buildString();
     }
 
-    public void setResult(String result) {
-        this.result = new StringBuilder(result);
-    }
-
-    public String negate() {
-        if (result.charAt(0) == '-') {
-            result.deleteCharAt(0);
+    public void negate() {
+        if (!currentEquation.isEmpty()) {
+            String lastElement = currentEquation.getLastElement().toString();
+            if (isNumber(lastElement)) {
+                if (lastElement.contains("-")) {
+                    currentEquation.getLastElement().deleteCharAt(0);
+                } else {
+                    currentEquation.appendToBeginningOfElement("-");
+                }
+            } else if (isOperation(lastElement)) {
+                if (lastElement.contains("-")) {
+                    if (isNumber(currentEquation.getOneBeforeLastElement())) {
+                        currentEquation.addNewElement("-");
+                    } else {
+                        currentEquation.removeLastElement();
+                    }
+                }
+            } else {
+                currentEquation.addNewElement("-");
+            }
         } else {
-            result.insert(0, '-');
+            currentEquation.addNewElement("-");
         }
-        return result.toString();
+        System.out.println("negate() invoked: " + currentEquation.getLastElement() + "   " + currentEquation.getSize());
     }
 
     public void addNumber(ButtonIdentifier btnId) {
-        //valid str according to result
-        if (validNumber(btnId)) {
-            result.append(btnId.getCharacter());
+        switch (validNumber(btnId)) {
+            case NO_ACTION:
+                break;
+            case APPEND_TO_ELEMENT:
+                currentEquation.appendToElement(btnId.getString());
+                break;
+            case ADD_NEW_ELEMENT:
+                currentEquation.addNewElement(btnId.getString());
+                break;
         }
-        setLastChar();
     }
 
-    private boolean validNumber(ButtonIdentifier btnId) {
+    private CalcValidatorAction validNumber(ButtonIdentifier btnId) {
+        CalcValidatorAction action;
         switch (btnId) {
             case ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE:
-                String lastNumber = getLastStringAfterRegex(result.toString(), "[+\\-*/]");
-                if (lastNumber.length() == 1 && lastNumber.equals("0")){
-                    return false;
+                if (currentEquation.isEmpty()) {
+                    action = CalcValidatorAction.ADD_NEW_ELEMENT;
                 } else {
-                    return true;
+                    String lastElement = currentEquation.getLastElement().toString();
+                    if (isNumber(lastElement)) {
+                        if (lastElement.charAt(0) == '0' && !lastElement.contains(",")) {
+                            action = CalcValidatorAction.NO_ACTION;
+                        } else {
+                            action = CalcValidatorAction.APPEND_TO_ELEMENT;
+                        }
+                    } else if (isOperation(lastElement) && lastElement.contains("-")) {
+                        if (isNumber(currentEquation.getOneBeforeLastElement())) {
+                            action = CalcValidatorAction.ADD_NEW_ELEMENT;
+                        } else {
+                            action = CalcValidatorAction.APPEND_TO_ELEMENT;
+                        }
+                    } else {
+                        action = CalcValidatorAction.ADD_NEW_ELEMENT;
+                    }
                 }
+                break;
             default:
-                return false;
+                action = CalcValidatorAction.NO_ACTION;
+                System.out.println("validNumber() invoked : " + action + "ATTENTION! default switch case used!");
+                return action;
         }
+        System.out.println("validNumber() invoked : " + action);
+        return action;
     }
 
     public void addOperation(ButtonIdentifier btnId) {
-        if (validOperation(btnId)) {
-            result.append(btnId.getCharacter());
+        switch (validOperation(btnId)) {
+            case NO_ACTION:
+                break;
+            case APPEND_TO_ELEMENT:
+                currentEquation.appendToElement(btnId.getString());
+                break;
+            case ADD_NEW_ELEMENT:
+                currentEquation.addNewElement(btnId.getString());
+                break;
         }
-        setLastChar();
     }
 
-    private boolean validOperation(ButtonIdentifier btnId) {
+    private CalcValidatorAction validOperation(ButtonIdentifier btnId) {
+        CalcValidatorAction action;
         switch (btnId) {
             case ADD, SUBTRACT, MULTIPLY, DIVIDE:
-                if (lastChar == null || "-+*/,".contains(lastChar.toString())) {
-                    return false;
+                if (isOperation(currentEquation.getLastElement())) {
+                    action = CalcValidatorAction.NO_ACTION;
                 } else {
-                    return true;
+                    action = CalcValidatorAction.ADD_NEW_ELEMENT;
                 }
+                break;
             default:
-                return false;
+                action = CalcValidatorAction.NO_ACTION;
+                System.out.println("validOperation() invoked : " + action + "ATTENTION! default switch case used!");
+                return action;
         }
+        System.out.println("validOperaion() invoked : " + action);
+        return action;
     }
 
     public void clearAll() {
@@ -137,7 +193,39 @@ public class CalcValidator {
     }
 
     private int getLastNumberBeginningIndex() {
-        String lastNumber = getLastStringAfterRegex(result.toString(), "[+\\-*/]");
+        String lastNumber = getLastStringAfterRegex(result.toString(), "[-+*/]");
         return result.length() - lastNumber.length() - 1;
+    }
+
+    private boolean isNumber(String element) {
+        if (element != null && element.matches(".*[0-9]+.*")) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isNumber(StringBuilder element) {
+        if (element != null) {
+            if (element.toString().matches(".*[0-9]+.*")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isOperation(String element) {
+        if (element != null && element.length() == 1 && element.matches("[-+/*]")) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isOperation(StringBuilder element) {
+        if (element != null) {
+            if (element.toString().length() == 1 && element.toString().matches("[-+/*]")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
