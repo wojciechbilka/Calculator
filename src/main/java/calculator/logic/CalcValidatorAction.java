19d28f01c9bddc620ce771bd4b3eb7b913c4e7c1
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-03-05 14:49:14 +0100

    Add new enum for proper messages from valid methods.

---
+++ src/main/java/calculator/logic/CalcValidatorAction.java
@@ -0,0 +1,10 @@
+package calculator.logic;
+
+public enum CalcValidatorAction {
+    NO_ACTION,
+    APPEND_TO_ELEMENT,
+    ADD_NEW_ELEMENT,
+    REMOVE_FROM_ELEMENT,
+    REMOVE_ENTIRE_ELEMENT,
+    ERROR;
+}
