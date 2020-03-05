775dfa362408a7fbb864b3516a07b135407d9ff3
Wojciech Biłka <wojciech.bilka@gmail.com>
2020-03-05 14:49:56 +0100

    Changed ButtonIdentifier field to String

--- src/main/java/calculator/logic/ButtonIdentifier.java
+++ src/main/java/calculator/logic/ButtonIdentifier.java
@@ -2,36 +2,36 @@ package calculator.logic;
 
 public enum ButtonIdentifier {
 
-    ZERO('0'),
-    ONE('1'),
-    TWO('2'),
-    THREE('3'),
-    FOUR('4'),
-    FIVE('5'),
-    SIX('6'),
-    SEVEN('7'),
-    EIGHT('8'),
-    NINE('9'),
-    COMMA(','),
-    ADD('+'),
-    SUBTRACT('-'),
-    MULTIPLY('*'),
-    DIVIDE('/'),
-    BACKSPACE('\b'),
-    CLEAR_ALL('c'),
-    CLEAR('c'),
-    NEGATE('\u00B1'),
-    EQUALS('=');
+    ZERO("0"),
+    ONE("1"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    COMMA(","),
+    ADD("+"),
+    SUBTRACT("-"),
+    MULTIPLY("*"),
+    DIVIDE("/"),
+    BACKSPACE("\b"),
+    CLEAR_ALL("c"),
+    CLEAR("c"),
+    NEGATE("\u00B1"),
+    EQUALS("=");
 
 
-    char character;
+    String str;
 
-    ButtonIdentifier(char character) {
-        this.character = character;
+    ButtonIdentifier(String str) {
+        this.str = str;
     }
 
-    public char getCharacter() {
-        return character;
+    public String getString() {
+        return str;
     }
 
 }
